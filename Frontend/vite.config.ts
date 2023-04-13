3c1e8bb79bfe86269115d8b9ab1544c59a056891
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-04-13 15:49:32 +0200

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    port: 5173
+  },
   build: {
     chunkSizeWarningLimit: 1600
   }
