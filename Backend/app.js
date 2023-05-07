082e5eeb9f9a8d9884365d7344d8c9a1dc190726
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2023-05-07 15:02:58 +0200

    Update backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,91 +2,88 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-setTimeout(() => {
-  // create database connection
-  const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-  db.connect((err) => {
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+
+const app = express();
+const port = 3000;
+const apiRouter = express.Router();
+
+app.use(bodyParser.json());
+app.use('/api', apiRouter);
+
+apiRouter.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+apiRouter.get('/tasks', (req, res) => {
+  db.query('SELECT * FROM Tasks', (err, result) => {
     if (err) throw err;
-    console.log('Connected to database');
+    res.send(result);
   });
+});
 
-  const app = express();
-  const port = 3000;
-  const apiRouter = express.Router();
-
-  app.use(bodyParser.json());
-  app.use('/api', apiRouter);
-
-  apiRouter.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
-  app.get('/tasks', (req, res) => {
-    db.query('SELECT * FROM Tasks', (err, result) => {
+apiRouter.get('/tasks/:id', (req, res) => {
+  db.query(
+    'SELECT * FROM Tasks WHERE id = ?',
+    [req.params.id],
+    (err, result) => {
       if (err) throw err;
-      res.send(result);
-    });
-  });
-
-  app.get('/tasks/:id', (req, res) => {
-    db.query(
-      'SELECT * FROM Tasks WHERE id = ?',
-      [req.params.id],
-      (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-          res.send(result[0]);
-        } else {
-          res.send('No task found');
-        }
+      if (result.length > 0) {
+        res.send(result[0]);
+      } else {
+        res.send('No task found');
       }
-    );
-  });
+    }
+  );
+});
 
-  app.get('/categories', (req, res) => {
-    db.query('SELECT * FROM Categories', (err, result) => {
+apiRouter.get('/categories', (req, res) => {
+  db.query('SELECT * FROM Categories', (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+apiRouter.get('/categories/:id', (req, res) => {
+  db.query(
+    'SELECT * FROM Categories WHERE id = ?',
+    [req.params.id],
+    (err, result) => {
       if (err) throw err;
-      res.send(result);
-    });
-  });
-
-  app.get('/categories/:id', (req, res) => {
-    db.query(
-      'SELECT * FROM Categories WHERE id = ?',
-      [req.params.id],
-      (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-          res.send(result[0]);
-        } else {
-          res.send('No category found');
-        }
+      if (result.length > 0) {
+        res.send(result[0]);
+      } else {
+        res.send('No category found');
       }
-    );
-  });
+    }
+  );
+});
 
-  app.get('/categories/:id/tasks', (req, res) => {
-    db.query(
-      'SELECT id, title, description, done FROM Tasks WHERE category_id = ?',
-      [req.params.id],
-      (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-          res.send(result);
-        } else {
-          res.send('No tasks found');
-        }
+apiRouter.get('/categories/:id/tasks', (req, res) => {
+  db.query(
+    'SELECT id, title, description, done FROM Tasks WHERE category_id = ?',
+    [req.params.id],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length > 0) {
+        res.send(result);
+      } else {
+        res.send('No tasks found');
       }
-    );
-  });
-  // start server
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-}, 10000);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
